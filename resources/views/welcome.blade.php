07d4888bb92cf2989dd74cdcd3bf3196c2d887c8
sorami.nakamura <soramilerning@gmail.com>
2019-12-31 02:41:01 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,38 @@
 
 @section('content')
     @if (Auth::check())
-        {{ Auth::user()->name }}
+        <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <img class="rounded img-fluid" src=
+                        "{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                @if (Auth::id() == $user->id)
+                    {!! Form::open(['route' => 'microposts.store']) !!}
+                        <div>
+                            {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                            {!! Form::submit('Post', ['class' => 'btn btn-info btn-block']) !!}
+                        </div>
+                    {!! Form::close() !!}
+                @endif
+                @if (count($microposts) > 0)
+                    @include('microposts.microposts', ['microposts' => $microposts])
+                @endif
+            </div>
+        </div>
     @else
     <div class="center jumbotron">　 <!-- トップ見出し -->
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-success']) !!} <!-- web.phpでつけた名前をURLに指定（signup.get）。これは、signup.postとの違いを区別している（どちらもURL自体は'login'） -->
+            {!! link_to_route('signup.get', 'Sign up now!', [], 
+            ['class' => 'btn btn-success']) !!} <!-- web.phpでつけた名前をURLに指定（signup.get）。これは、signup.postとの違いを区別している（どちらもURL自体は'login'） -->
         </div>
     </div>
     @endif
